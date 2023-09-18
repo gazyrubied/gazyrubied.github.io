18de47fa4010c92eb65a678a3ec4ce58db8ae1d2
gazyrubied <rivergaz4@gmail.com>
2023-09-17 23:39:54 -0400

    Added assingment 6

---
+++ assignments/assignment5/script.js
@@ -0,0 +1,17 @@
+const showGhost = () => {
+    document.getElementById("ghost").classList.remove("hide");
+}
+
+const hideGhost = () => {
+    document.getElementById("ghost").classList.add("hide"); 
+}
+
+const makeMove = () => {
+    document.getElementById("emoji").classList.add("moving");
+}
+
+window.onload = () => {
+    document.getElementById("button-show").onclick = showGhost;
+    document.getElementById("button-hide").onclick = hideGhost;
+    document.getElementById("button-move").onclick = makeMove;
+}

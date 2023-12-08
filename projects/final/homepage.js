65ea0972af381e85d6a30d5ed1c8951677881cab
gazyrubied <rivergaz4@gmail.com>
2023-12-08 10:50:37 -0500

    added psoters

--- projects/final/homepage.js
+++ projects/final/homepage.js
@@ -1,5 +1,5 @@
 const getMovies = async () => {
-    const link = "https://gazyrubied.github.io/projects/part5/json/movies.json";
+    const link = "https://gazyrubied.github.io/projects/final/json/movies.json";
 
     try {
         const response = await fetch(link);
@@ -17,31 +17,82 @@ const displayMovies = async () => {
         const movieDiv = document.createElement('div');
         movieDiv.classList.add('movie-info');
         const link = document.createElement('a');
-        const image = document.createElement('img'); // Change here
-        const title = document.createElement('h2');
-        const director = document.createElement('p');
-        const cast = document.createElement('p');
-        const year = document.createElement('p');
+        const image = document.createElement('img');
+        const title = movie.title;
+        const director = movie.Director;
+        const cast = movie.Actors;
+        const year = movie.Year;
+        const rated = movie.Rated;
+        const runtime = movie.Runtime;
 
-        link.href = 'review.html';
+        link.href = '#'; // Use '#' as a placeholder for the modal
         image.src = movie.image;
         image.alt = `${movie.title} Movie Poster`;
         image.classList.add('movie-image');
 
-        title.textContent = movie.title;
-        director.textContent = `Director: ${movie.director}`;
-        cast.textContent = `Cast: ${movie.cast.join(', ')}`;
-        year.textContent = `Year: ${movie.year}`;
-
-        title.textContent = movie.title;
-
         link.appendChild(image);
-        movieDiv.appendChild(link); 
-        movieDiv.appendChild(title);
-        movieDiv.appendChild(director);
-        movieDiv.appendChild(cast);
-        movieDiv.appendChild(year);
+        movieDiv.appendChild(link);
         moviesContainer.appendChild(movieDiv);
-        
+
+        // Add click event listener for each movie image
+        image.addEventListener('click', () => {
+            // Show modal and display movie information
+            showModal(title, director, cast, year, rated, runtime);
+        });
     });
 };
+
+// Function to show the modal and display movie information
+const showModal = (title, director, cast, year, rated, runtime) => {
+    const modal = document.getElementById('myModal');
+    const modalContent = document.getElementById('modal-content');
+
+    // Clear previous content
+    modalContent.innerHTML = '';
+
+    // Create elements to display in the modal
+    const titleElement = document.createElement('h2');
+    titleElement.textContent = title;
+
+    const directorElement = document.createElement('p');
+    directorElement.textContent = `Director: ${director}`;
+
+    const castElement = document.createElement('p');
+    castElement.textContent = `Cast: ${cast}`;
+
+    const yearElement = document.createElement('p');
+    yearElement.textContent = `Year: ${year}`;
+
+    const ratedElement = document.createElement('p');
+    ratedElement.textContent = `Rated: ${rated}`;
+
+    const runtimeElement = document.createElement('p');
+    runtimeElement.textContent = `Runtime: ${runtime}`;
+
+    // Append elements to modal content
+    modalContent.appendChild(titleElement);
+    modalContent.appendChild(directorElement);
+    modalContent.appendChild(castElement);
+    modalContent.appendChild(yearElement);
+    modalContent.appendChild(ratedElement);
+    modalContent.appendChild(runtimeElement);
+
+    // Display the modal
+    modal.style.display = 'block';
+
+    // Close the modal when the 'x' is clicked
+    const closeModal = document.getElementsByClassName('close')[0];
+    closeModal.onclick = function () {
+        modal.style.display = 'none';
+    };
+
+    // Close the modal when clicking outside of it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    };
+};
+
+
+window.onload = displayMovies;
